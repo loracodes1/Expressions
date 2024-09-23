f553f56a91e3dc98c90d803e5a6768796998188c
florence <loracodes3@gmail.com>
2024-09-23 13:34:41 +0300

    mutability in variables

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 9000 +10000 //constant expression
 maximumSpeed =9000; //assignment expression
+
+//mutability
+heightInCentimeters=100;
+heightInCentimeters=180; //the console will return this as the value of heightInCentimeters because its the last value assigned to the variable
